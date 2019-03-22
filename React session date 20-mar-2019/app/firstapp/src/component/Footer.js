d340f0ed04e213700f528e170f2824c8f7982647
kamal <kamal.pandey@tothenew.com>
2019-03-22 12:41:11 +0530

    Adding assignment ES6 session 2 date 15 march 2019!

---
+++ React session date 20-mar-2019/app/firstapp/src/component/Footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+class Footer extends Component{
+    render() {
+        return(
+            <footer>Footer</footer>
+        )
+    }
+}
+
+export default Footer;
